153e326b8ca31f391ca607f7a8c75de03acd9be2
sanish <sanish.kumar@weddingasia.in>
2018-12-16 19:38:13 +0530

    added readme.md instructions and some comments

---
+++ Readme.md
@@ -0,0 +1,5 @@
+** Instructions
+* npm install
+* npm start for react app
+* node .\src\server\server.js for API server running on 3001 port
+* mongodump is in mongodump folder for sample data
--- src/server/api.js
+++ src/server/api.js
@@ -7,19 +7,37 @@ const Feed = require('./models/Feed');
 const User = require('./models/User');
 
 app.use(cors()); // to send CORS headers.
-// app.use(express.urlencoded()); // to support URL-encoded bodies.
 app.use(bodyParser.urlencoded({ extended: true })); // Parses urlencoded bodies
 app.use(bodyParser.json()); // Send JSON responses
 
-// let Feeds = [];
+/**
+ * Yet to make
+ */
 apiRoute.get("/addUser",(req,res)=>{
   // console.log(req)
-  // User.find({"email" : "dfss@ddzff.dfa"},function(err,UserList){
-  //   console.log(UserList);
-  // });
+  
   res.send(req.params)
 });
 
+/**
+ * This API returns all feed based on user's email
+ * sample request: http://localhost:3001/getAllFeedsbasedOnUser/sanishkr@gmail.com
+ * sample response: [
+  {
+  _id: "5c16372c77d8589a420d54df",
+  stats: {
+  createdAt: "2018-02-25T10:52:38.265Z",
+  feedStatus: true
+  },
+  videos: [ ],
+  link: {
+  imageWidth: "100",
+  imageHeight: "100",
+  islinkImage: false,
+  linkURL: "https://pbs.twimg.com/profile_images/634744823522717696/GIsiyzHp_400x400.png"
+  }
+]
+ */
 apiRoute.get("/getAllFeedsbasedOnUser/:email",(req,res,next)=>{
   if (!req.params.email) {
     res.status(422).send({ error: 'Please choose any email' });
@@ -43,14 +61,13 @@ apiRoute.get("/getAllFeedsbasedOnUser/:email",(req,res,next)=>{
   // res.send(JSON.stringify(Feeds.find({"_id":"5c16372c77d8589a420d54df"})));
 });
 
+/**
+ * Yet to make
+ */
 apiRoute.post("/addFeed",(req,res)=>{
   //console.log(req.body)
-  let m = req.body
-  //Feeds.push(m)
-  //console.log(Feeds)
   let msg = {"message":"Feed Added","status":true};
   res.send(msg)
-  // res.send(m)
 });
 
 app.use("/", apiRoute);
